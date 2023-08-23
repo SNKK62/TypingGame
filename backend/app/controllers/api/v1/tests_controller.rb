a35329b280db0bd065a9c998cb2abe89dad36135
SNKK62 <koki.seno62@gmail.com>
2023-08-23 23:51:03 +0900

    remove wasted files

--- backend/app/controllers/api/v1/tests_controller.rb
+++
@@ -1,9 +0,0 @@
-module Api
-    module V1
-        class TestsController < ApplicationController
-            def index
-                render json: {name: 'taro', age: 12}
-            end
-        end
-    end
-end
