3aa8cf80ab34b5776a9b61b387f7c056d25f59ad
SNKK62 <koki.seno62@gmail.com>
2023-09-20 22:57:38 +0900

    text componentの実装

---
+++ frontend/src/components/text.tsx
@@ -0,0 +1,20 @@
+import { Typography } from '@mui/material';
+interface TextProps {
+  position?: 'center' | 'inherit' | 'justify' | 'left' | 'right';
+  children: React.ReactNode;
+  htmlType?: 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6' | 'p';
+}
+
+export const Text: React.FC<TextProps> = ({
+  children,
+  position = 'inherit',
+  htmlType = 'p',
+}) => {
+  const variant = htmlType === 'p' ? 'body1' : htmlType;
+  const style = { width: '100%' };
+  return (
+    <Typography align={position} variant={variant} sx={style}>
+      {children}
+    </Typography>
+  );
+};
