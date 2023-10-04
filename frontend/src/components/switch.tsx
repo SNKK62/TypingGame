08b4c3d2a273572492f7626c0982671d7dbc9599
takagiyusuke <yusuke.10.06@outlook.jp>
2023-10-05 08:50:34 +0900

    switchの修正(2023/10/5 8:50)

--- frontend/src/components/switch.tsx
+++ frontend/src/components/switch.tsx
@@ -1,8 +1,8 @@
 import RawSwitch from '@mui/material/Switch';
-import { useState } from 'react';
 
 interface SwitchProps {
   // checkedIcon?: React.ReactNode;
+  checked: boolean;
   color?:
     | 'default'
     | 'primary'
@@ -11,62 +11,35 @@ interface SwitchProps {
     | 'info'
     | 'success'
     | 'warning';
-  defaultChecked?: boolean;
   disabled?: boolean;
   edge?: 'end' | 'start' | false;
   // icon?: string;
   id: string;
   onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
   size?: 'medium' | 'small';
-  forcedOff?: boolean;
-  forcedOn?: boolean;
-  forcedDisabled?: boolean;
-  forcedAbled?: boolean;
 }
 
 export const Switch: React.FC<SwitchProps> = ({
   // checkedIcon,
+  checked,
   color = 'secondary',
-  defaultChecked = false,
   disabled = false,
   edge = false,
   // icon,
   id,
   size = 'medium',
-  forcedOff = false,
-  forcedOn = false,
-  forcedDisabled = false,
-  forcedAbled = false,
   onChange = () => {},
 }) => {
-  const [checked, setChecked] = useState(defaultChecked);
-  const [disablity, setDisablity] = useState(disabled);
-  const onSwitch = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setChecked(event.target.checked);
-  };
-  if (forcedOn && !checked) {
-    setChecked(true);
-  }
-  if (forcedOff && checked) {
-    setChecked(false);
-  }
-  if (forcedDisabled && !disablity) {
-    setDisablity(true);
-  }
-  if (forcedAbled && disablity) {
-    setDisablity(false);
-  }
   return (
     <RawSwitch
       checked={checked}
       // checkedIcon={checkedIcon}
       color={color}
-      disabled={disablity}
+      disabled={disabled}
       edge={edge}
       // icon={icon}
       id={id}
       onChange={(e) => {
-        onSwitch(e);
         onChange(e);
       }}
       size={size}
