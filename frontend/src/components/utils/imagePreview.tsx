26aaf92b21af9656329b47f37540769a9b5866b7
takagiyuusuke <142160956+takagiyuusuke@users.noreply.github.com>
2023-11-12 13:53:23 +0900

    インプットコンポーネントの暫定的な完成 (#10)
    * インプットコンポーネントの暫定的な完成
    
    * imagepreviewを分離化
    
    * review 対応
    
    * complete

---
+++ frontend/src/components/utils/imagePreview.tsx
@@ -0,0 +1,41 @@
+import NextImage from 'next/image';
+
+interface ImagePreviewProps {
+  image: string | undefined;
+  alt?: string;
+  maxWidth?: number;
+  maxHeight?: number;
+}
+
+export const ImagePreview: React.FC<ImagePreviewProps> = ({
+  image,
+  alt = 'preview',
+  maxWidth = 400,
+}) => {
+  return (
+    <div
+      style={{ maxWidth: `${maxWidth}px`, width: '100%', overflow: 'hidden' }}
+    >
+      <div style={{ paddingTop: '100%', position: 'relative' }}>
+        <div
+          style={{
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            bottom: 0,
+            right: 0,
+          }}
+        >
+          {image && (
+            <NextImage
+              src={image}
+              alt={alt}
+              layout='fill'
+              objectFit='contain'
+            />
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
