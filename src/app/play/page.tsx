e40d2b99342b0c51827b9d3c090f5d8b8e49e0a4
takagiyuusuke <yusuke.10.06@outlook.jp>
2024-04-29 21:01:23 +0900

    titleの変更

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,12 +1,5 @@
 import { PlayContainer } from './container';
 
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
-  title: 'Typing Game',
-  description: 'キー配置を覚えながら楽しくタイピングゲームを遊べます！',
-};
-
 const Page = () => (
   <>
     <PlayContainer />
