5fd97656c63ca1f4ebfeb78a0b1bae1b4a392020
takagiyuusuke <yusuke.10.06@outlook.jp>
2024-05-10 17:35:45 +0900

    存在しないidへアクセスした際の対応を簡単に追加

--- src/app/play/[id]/container.tsx
+++ src/app/play/[id]/container.tsx
@@ -7,5 +7,13 @@ interface Props {
 }
 export const PlayContainer = async ({ id }: Props) => {
   const data = await getWords(id);
-  return <Presenter data={data} />;
+  console.log('データ！！：' + data);
+  if (data?.length !== 0) {
+    return <Presenter data={data} />;
+  }
+  return (
+    <div style={{ height: '100vh', width: '100vw' }}>
+      <h2>指定したidのグループが存在しません</h2>
+    </div>
+  );
 };
